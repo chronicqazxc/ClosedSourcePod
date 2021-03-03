5f814865767ba5f977ddb1c38dfb2f3dc809f7b5
Wayne Hsiao <wayne.hsiao@candyhouse.co>
2021-03-03 14:06:27 +0900

    add framework

---
+++ ClosedCodeFramework.framework/Headers/ClosedCodeFramework.h
@@ -0,0 +1,18 @@
+//
+//  ClosedCodeFramework.h
+//  ClosedCodeFramework
+//
+//  Created by YuHan Hsiao on 2021/03/03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClosedCodeFramework.
+FOUNDATION_EXPORT double ClosedCodeFrameworkVersionNumber;
+
+//! Project version string for ClosedCodeFramework.
+FOUNDATION_EXPORT const unsigned char ClosedCodeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClosedCodeFramework/PublicHeader.h>
+
+
